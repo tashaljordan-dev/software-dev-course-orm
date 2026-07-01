d9f182b98248d54795b22c3a87bfa4079ad77297
tashaljordan-dev <tashaljordan@gmail.com>
2026-07-01 18:38:54 -0500

    done
    My pom.xml file does not work. I tried for hours.

---
+++ src/main/java/com/example/orm_exercise/models/Address.java
@@ -0,0 +1,75 @@
+package com.example.orm_exercise.models;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class Address {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String street;
+    private String city;
+    private String state;
+    private String zipCode;
+
+    @ManyToOne
+    @JoinColumn(name = "contact_id")
+    @JsonIgnore
+    private Contact contact;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(String zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    public Contact getContact() {
+        return contact;
+    }
+
+    public void setContact(Contact contact) {
+        this.contact = contact;
+    }
+}
--- src/main/java/com/example/orm_exercise/models/Contact.java
+++ src/main/java/com/example/orm_exercise/models/Contact.java
@@ -4,6 +4,10 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany; 
+import java.util.ArrayList;
+import java.util.List;
+import jakarta.persistence.CascadeType;
 
 @Entity
 public class Contact {
@@ -13,6 +17,16 @@ public class Contact {
     private String name;
     private String email;
     private String phoneNumber;
+
+    // add address fields with one to many relationship 
+
+    @OneToMany (
+        mappedBy = "contact", 
+        cascade = CascadeType.ALL, 
+        orphanRemoval = false
+    )
+
+    private List<Address> addresses = new ArrayList<>();
 
     public int getId() {
         return id;
@@ -53,5 +67,26 @@ public class Contact {
         this.name = name;
         this.email = email;
         this.phoneNumber = phoneNumber;
+    }
+
+    // add getters and setters for addresses 
+    
+    public List <Address> getAddresses() {
+        return addresses;
+    }
+
+    public void setAddresses(List<Address> addresses) {
+        this.addresses = addresses;
+    }
+
+    // added helper methods to add and remove addresses
+    public void addAddress(Address address) {
+        addresses.add(address);
+        address.setContact(this);
+    }
+
+    public void removeAddress(Address address) {
+        addresses.remove(address);
+        address.setContact(null);
     }
 }
